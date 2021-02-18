9c1f99f05bf984b049c88050996fb3e74c42159c
gusailov <gusailov@gmail.com>
2021-02-18 23:33:36 +0200

    add_answers_question_id_null_constraint

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_18_212141) do
+ActiveRecord::Schema.define(version: 2021_02_18_213246) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
-    t.integer "question_id"
+    t.integer "question_id", null: false
     t.boolean "true_false"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
