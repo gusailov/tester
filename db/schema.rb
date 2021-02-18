e3ec4a7c87a1046c4205c277f638be44033e427f
gusailov <gusailov@gmail.com>
2021-02-18 23:18:55 +0200

    add_questions_body_null_constraint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_18_205508) do
+ActiveRecord::Schema.define(version: 2021_02_18_211459) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2021_02_18_205508) do
   end
 
   create_table "questions", force: :cascade do |t|
-    t.text "body"
+    t.text "body", null: false
     t.integer "test_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
