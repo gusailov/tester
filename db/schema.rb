4274965c19c990a48550d3fc7178d0c70553d772
gusailov <gusailov@gmail.com>
2021-02-18 23:40:02 +0200

    add_users_name_null_constraint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_18_213539) do
+ActiveRecord::Schema.define(version: 2021_02_18_213911) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 2021_02_18_213539) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.string "surname"
     t.date "dob"
     t.string "role"
