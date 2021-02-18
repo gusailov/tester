e728dc36bd94cd6034f37b4f9704f6decc65d6be
gusailov <gusailov@gmail.com>
2021-02-18 22:38:16 +0200

    answer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_18_202647) do
+ActiveRecord::Schema.define(version: 2021_02_18_203653) do
+
+  create_table "answers", force: :cascade do |t|
+    t.text "body"
+    t.integer "question_id"
+    t.boolean "true_false"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.text "body"
