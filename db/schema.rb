fc241d26713821aa03fa76ae4720e67103c146ff
gusailov <gusailov@gmail.com>
2021-02-18 22:27:30 +0200

    question

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_17_144105) do
+ActiveRecord::Schema.define(version: 2021_02_18_202647) do
+
+  create_table "questions", force: :cascade do |t|
+    t.text "body"
+    t.integer "test_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
