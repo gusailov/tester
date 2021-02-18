fb65b69e1ca6cc0b3eca6c7ab67c14087b959868
gusailov <gusailov@gmail.com>
2021-02-18 22:52:16 +0200

    user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_18_203653) do
+ActiveRecord::Schema.define(version: 2021_02_18_205156) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 2021_02_18_203653) do
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
     t.integer "level"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "surname"
+    t.date "dob"
+    t.string "role"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
