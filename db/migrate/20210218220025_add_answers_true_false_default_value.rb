0d26b624c3bca8612cbfa5340bf5105e43995121
gusailov <gusailov@gmail.com>
2021-02-19 08:42:32 +0200

    defaulr values

--- db/migrate/20210218220025_add_answers_true_false_default_value.rb
+++ db/migrate/20210218220025_add_answers_true_false_default_value.rb
@@ -1,5 +1,5 @@
 class AddAnswersTrueFalseDefaultValue < ActiveRecord::Migration[6.1]
   def change
-    change_column_default(:answers, :true_false, true)
+    change_column_default(:answers, :true_false, from: nil, to: true)
   end
 end
--- db/migrate/20210218220742_add_answers_true_false_default_val.rb
+++
@@ -1,5 +0,0 @@
-class AddAnswersTrueFalseDefaultVal < ActiveRecord::Migration[6.1]
-  def change
-    change_column_default(:answers, :true_false, from: nil, to: true)
-  end
-end
