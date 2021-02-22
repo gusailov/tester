468d2eb5b4e6d2ca8eaca0aa003ba8176f43f507
gusailov <gusailov@gmail.com>
2021-02-23 00:15:05 +0200

    test_user_methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,9 @@
 class Test < ApplicationRecord
+  belongs_to :category
+
+  def self.tests_by_category(category_title)
+    category = Category.find_by title: category_title
+    category_id = category.id
+    Test.select(:title).where(category_id: category_id).order(title: :desc)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  has_many :tests
+
+  scope :user_tests, ->(level) { where('level= ?', level) }
+  def user_tests(level)
+    tests = Test.where(level: level)
+    User.pluck(:results)
+    Result.joins(:user_id)
+  end
 end
