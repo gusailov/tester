71b5542e68e9ff98bba42dd808d2c22fd4a60f83
gusailov <gusailov@gmail.com>
2021-02-23 08:57:35 +0200

    lesson_4_1.1

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,5 @@
 class Test < ApplicationRecord
-  belongs_to :category
-
+  
   def self.tests_by_category(category_title)
     category = Category.find_by title: category_title
     Test.select(:title).where(category_id: category.id).order(title: :desc)
