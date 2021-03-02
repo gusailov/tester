9628e86358bf7fc5bed9e6d3d5c1d0eb4de96e92
gusailov <gusailov@gmail.com>
2021-03-02 12:49:38 +0200

    add_email_uniq_index_to_user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_01_213848) do
+ActiveRecord::Schema.define(version: 2021_03_02_103417) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2021_03_01_213848) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "answers", "questions"
