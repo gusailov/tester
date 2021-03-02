96102b953005150387c4074aeaa87ab170e2465e
gusailov <gusailov@gmail.com>
2021-03-02 16:05:31 +0200

    add_title_feat_level_uniq_index_to_test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_115350) do
+ActiveRecord::Schema.define(version: 2021_03_02_140248) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2021_03_02_115350) do
     t.integer "author_id"
     t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
   end
 
   create_table "users", force: :cascade do |t|
