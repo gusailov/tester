3c1d46b8609cfc073ee27561af680ced6349c894
gusailov <gusailov@gmail.com>
2021-03-03 14:45:54 +0200

    show_question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,12 @@
 class QuestionsController < ApplicationController
   def index
-    # render json: { questions: Question.all }
-    result = ["Params: #{params.inspect}, test_id: #{params[:test_id]}"]
     render plain: Test.find(params[:test_id]).questions.inspect
   end
 
   def show
+    render plain: Question.find(params[:id]).inspect
+  end
+
+  def create
   end
 end
