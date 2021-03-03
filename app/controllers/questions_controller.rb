a99231d3015b2377f14f894c486c03ca21e96627
gusailov <gusailov@gmail.com>
2021-03-03 12:13:36 +0200

    questions_from_test

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,10 @@
 class QuestionsController < ApplicationController
+  def index
+    # render json: { questions: Question.all }
+    result = ["Params: #{params.inspect}, test_id: #{params[:test_id]}"]
+    render plain: Test.find(params[:test_id]).questions.inspect
+  end
+
+  def show
+  end
 end
