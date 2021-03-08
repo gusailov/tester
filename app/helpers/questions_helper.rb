1c188a3613f397814c4a2d539e0e977d8881fd50
gusailov <gusailov@gmail.com>
2021-03-09 00:57:56 +0200

    tsak_3

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,13 @@
 module QuestionsHelper
+  def question_header(test)
+    "#{params[:action]} '#{test.title}' question:"
+  end
+
+  def current_year
+    Time.now.strftime("%Y")
+  end
+
+  def github_url(author, repo)
+    "https://github.com/#{author}/#{repo}"
+  end
 end
