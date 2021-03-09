a2f3e18ad4da945c4660286b33fd2bb9d815a81f
gusailov <gusailov@gmail.com>
2021-03-09 22:47:49 +0200

    user_create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    # render plain: "#{user_params}"
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :surname, :dob, :email, :role)
+    #params[:]
+  end
 end
