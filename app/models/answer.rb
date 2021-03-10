3f5be78ebc5e5358ea6cb46a60e8abf28e47d8e6
gusailov <gusailov@gmail.com>
2021-03-10 18:14:49 +0200

    lesson_8_5

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,13 +2,13 @@ class Answer < ApplicationRecord
   belongs_to :question, inverse_of: :answers
 
   validates :body, presence: true
-  validate  :validate_answers_count
+  validate :validate_answers_count, on: :create
 
   scope :correct_answer, -> { where(correct: true) }
 
   private
 
   def validate_answers_count
-    errors.add(:base, 'you can add only 1..4 answers') if question.answers.count >= 4
+    errors.add(:base, "you can add only 1..4 answers") if question.answers.count >= 4
   end
 end
