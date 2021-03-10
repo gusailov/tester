5ac1a5a33b73c7bbd31ab508a38b631fe678070b
gusailov <gusailov@gmail.com>
2021-03-10 08:48:02 +0200

    category_control

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,29 @@
 class CategoriesController < ApplicationController
+  before_action :find_category, only: %i[show]
+
+  def new
+    @category = Category.new
+  end
+
+  def show
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_category
+    @categories = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:title)
+  end
 end
