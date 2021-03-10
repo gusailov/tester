a9feed46f28f8493ece34a1dc6c26a79f8c659a5
gusailov <gusailov@gmail.com>
2021-03-10 13:06:44 +0200

    add_all_categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,14 @@
 class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+
   def new
     @category = Category.new
+  end
+
+  def show
+    @category = Category.find(params[:id])
   end
 
   def create
