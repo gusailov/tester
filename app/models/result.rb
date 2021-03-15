b791cbe97e327a26de7c1d54ec138cd0b79f2b00
gusailov <gusailov@gmail.com>
2021-03-15 09:44:09 +0200

    set next question with callback_2

--- app/models/result.rb
+++ app/models/result.rb
@@ -6,7 +6,7 @@ class Result < ApplicationRecord
   validates :user_id, uniqueness: { scope: :test_id, message: "you cannot pass this test twice" }
 
   before_validation :before_validation_set_first_question, on: :create
-  before_save :before_save_set_next_question
+  before_update :before_update_set_next_question
 
   def completed?
     current_question.nil?
@@ -20,6 +20,10 @@ class Result < ApplicationRecord
     save!
   end
 
+  def success?
+    (self.correct_questions / test.questions.count) * 100 >= 85
+  end
+
   private
 
   def before_validation_set_first_question
@@ -27,7 +31,6 @@ class Result < ApplicationRecord
   end
 
   def correct_answer?(answer_ids)
-    puts answer_ids
     correct_answers.ids.sort == answer_ids.map(&:to_i).sort
   end
 
@@ -35,7 +38,7 @@ class Result < ApplicationRecord
     current_question.answers.correct_answer
   end
 
-  def before_save_set_next_question
+  def before_update_set_next_question
     self.current_question = test.questions.order(:id).where('id>?', current_question.id).first
   end
 end
