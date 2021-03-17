4e7d7233ab03a8236ecdc2d05e928d860d14d4b2
gusailov <gusailov@gmail.com>
2021-03-17 14:30:17 +0200

    tsak_1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, except: %i[new create]
   before_action :find_user, only: %i[show edit update destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_user_not_found
@@ -17,6 +18,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_path
     else
       render :new
