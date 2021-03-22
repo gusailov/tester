05fb88ceb0bf1ccca44895a0f0aecb981719598b
gusailov <gusailov@gmail.com>
2021-03-22 23:05:26 +0200

    install devise login sign_up

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'tests#index'
 
-  get :signup, to: 'users#new'
-  get :login, to: 'sessions#new'
-  delete :logout, to: 'sessions#destroy'
+  devise_for :users, path_names: { sign_in: :login, sign_out: :logout }
 
-  resources :categories, :users, :test_passages
-  resources :sessions, only: :create
+  resources :categories, :test_passages
 
   resources :tests do
     resources :questions, shallow: true do
