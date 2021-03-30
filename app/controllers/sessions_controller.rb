38ff6684e385d175324e9879604df9c22ab3aad6
gusailov <gusailov@gmail.com>
2021-03-30 12:34:57 +0300

    fix 6.0

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,11 @@
 class SessionsController < Devise::SessionsController
-  def create
-    super
-    flash[:notice] = "Hello, #{current_user.name}"
-  end
+  after_action :greeting, only: :create
 
   private
+
+  def greeting
+    flash[:notice] = "Hello, #{current_user.name}"
+  end
 
   def after_sign_in_path_for(current_user)
     if current_user.admin?
