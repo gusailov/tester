c4345621c45db626f1c31f27e9e3cec65a0f467b
gusailov <gusailov@gmail.com>
2021-04-01 21:38:39 +0300

    test passage start trsnslation

--- app/helpers/test_passages_helper.rb
+++
@@ -1,5 +0,0 @@
-module TestPassagesHelper
-  def success_message(result)
-    result.success? ? "Test #{result.test.title} completed with" : "Test #{result.test.title} NOT completed with"
-  end
-end
