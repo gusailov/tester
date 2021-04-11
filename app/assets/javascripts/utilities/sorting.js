d401a865ae03b247721f4eac07d4d39945001fb3
gusailov <gusailov@gmail.com>
2021-04-11 22:04:49 +0300

    add tbody to table

--- app/assets/javascripts/utilities/sorting.js
+++ app/assets/javascripts/utilities/sorting.js
@@ -1,9 +1,49 @@
 document.addEventListener("turbolinks:load", function () {
   let control = document.querySelector(".sort-by-title");
-  control.addEventListener("click", sortRowsByTitle);
+
+  if (control) {
+    console.log("control", control);
+    control.addEventListener("click", sortRowsByTitle);
+  }
 });
 
 function sortRowsByTitle() {
   let table = document.querySelector("table");
-  console.log("Hello Ruby", table);
+  let rows = table.querySelectorAll("tr");
+  let sortedRows = [];
+
+  for (let i = 1; i < rows.length; i++) {
+    sortedRows.push(rows[i]);
+  }
+  sortedRows.sort(compareRows);
+
+  let sortedTable = document.createElement("table");
+  let body = sortedTable.createTBody();
+
+  sortedTable.classList.add(
+    "table",
+    "table-success",
+    "table-striped",
+    "align-middle"
+  );
+
+  body.appendChild(rows[0]);
+
+  for (let i = 0; i < sortedRows.length; i++) {
+    body.appendChild(sortedRows[i]);
+  }
+
+  table.parentNode.replaceChild(sortedTable, table);
+}
+
+function compareRows(row1, row2) {
+  let testTitle1 = row1.querySelector("td").textContent;
+  let testTitle2 = row2.querySelector("td").textContent;
+  if (testTitle1 < testTitle2) {
+    return -1;
+  }
+  if (testTitle1 > testTitle2) {
+    return 1;
+  }
+  return 0;
 }
