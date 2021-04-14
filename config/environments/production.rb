8d05176215e822b72986cec2c8cb8a7a7accdbbc
gusailov <gusailov@gmail.com>
2021-04-14 15:13:39 +0300

    action_mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "tester_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'enigmatic-bastion-68077.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
