4f996339fe43d1c7fd04646cf1159be692cbb208
gusailov <gusailov@gmail.com>
2021-04-23 16:11:10 +0300

    try find categories

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -10,6 +10,7 @@ class TestPassagesController < ApplicationController
 
     if @test_passage.completed?
       TestsMailer.completed_test(@test_passage).deliver_now
+      current_user.reward_check
       redirect_to result_test_passage_path(@test_passage)
     else
       render :show
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,8 +19,6 @@ class User < ApplicationRecord
                     format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   validates :name, presence: true
 
-  after_update :after_update_reward_check
-
   def test_passage(test)
     test_passages.order(created_at: :desc).find_by(test_id: test.id)
   end
@@ -29,17 +27,21 @@ class User < ApplicationRecord
     is_a?(Admin)
   end
 
-  def after_update_reward_check
-    success_tests = test_passages.filter { |tp| tp.success? }
+  def reward_check
     rewards = Reward.all
-
-    if success_tests.length == 1
-      reward = rewards.success_test_count.find_by rule_value: '1'
-      users_rewards.create(reward_id: reward.id)
+    rewards.each do |reward|
+      case
+      when success_test_passages.length.to_s == reward.rule_value
+        users_rewards.create(reward_id: reward.id)
+      end
     end
-    if success_tests.length == 3
-      reward = rewards.success_test_count.find_by rule_value: '5'
-      users_rewards.create(reward_id: reward.id)
+  end
+
+  def success_tests
+    success_test_passages = test_passages.filter { |tp| tp.success? }
+
+    success_test_passages.each do |t|
+      tests << t.test
     end
   end
 
