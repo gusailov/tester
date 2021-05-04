6b1b0ae92fe9d17f7d37606812579bbf51145e3b
gusailov <gusailov@gmail.com>
2021-05-04 13:11:39 +0300

    countdown ready

--- app/assets/javascripts/utilities/countdown.js
+++ app/assets/javascripts/utilities/countdown.js
@@ -1,7 +1,6 @@
 document.addEventListener("turbolinks:load", function () {
   let control = document.querySelector("#countdown");
   if (!control) {
-    clearInterval();
     sessionStorage.setItem("time", 0);
   }
   if (control) {
@@ -12,17 +11,26 @@ document.addEventListener("turbolinks:load", function () {
         : sessionStorage.getItem("time");
 
     function countdownUpdate() {
+      if (time <= 0) {
+        redirect();
+      }
       let minutes = Math.floor(time / 60);
       let seconds = time % 60;
       seconds = seconds < 10 ? "0" + seconds : seconds;
       minutes = minutes < 10 ? "0" + minutes : minutes;
       control.textContent = `${minutes}:${seconds}`;
       time--;
-      if (time <= 0) {
-        clearInterval((time = 0));
-      }
       sessionStorage.setItem("time", time);
     }
-    setInterval(countdownUpdate, 100);
+    let countdown = setInterval(countdownUpdate, 100);
+    function redirect() {
+      time = 0;
+      clearInterval(countdown);
+      setTimeout(() => {
+        url = window.location.href;
+        console.log(`${url}/result`);
+        // window.location.assign(`${url}/result`);
+      }, 2000);
+    }
   }
 });
