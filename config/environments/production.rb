1efad55a6da29d93d3ab0581bd283f911991d8b2
gusailov <gusailov@gmail.com>
2021-10-06 11:09:28 +0300

    smtp_host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "tester_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'tester-gusailov.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: ENV['SMTP_HOST'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
